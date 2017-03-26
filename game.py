62794b4d8c45fa339b4284f9f789a080bc55cdd8
Roel Huybrechts <roel@huybrechts.re>
2017-03-26 17:19:13 +0200

    Make it work.

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
 from qgis.core import *
 from qgis.gui import *
 
@@ -29,34 +32,87 @@ class Game(object):
 
         self.featureIdsGuessed = []
         self.featureToGuess = None
+        self.score = 0
 
         self.mapToolPan = QgsMapToolPan(self.iface.mapCanvas())
         self.mapToolPoint = QgsMapToolEmitPoint(self.iface.mapCanvas())
 
+        self.transformWGStoMap = QgsCoordinateTransform(
+            QgsCoordinateReferenceSystem(4326, QgsCoordinateReferenceSystem.EpsgCrsId),
+            self.iface.mapCanvas().mapRenderer().destinationCrs())
+
     def setNextFeature(self):
-        self.featureToGuess = getRandomFeature(self.layer, self.featureIdsGuessed)
-        self.dockwidget.nameLabel.setText(str(self.featureToGuess.attribute("name")))
+        if self.rounds > 0:
+            self.rounds -= 1
+
+            self.featureToGuess = getRandomFeature(self.layer, self.featureIdsGuessed)
+            self.pointToGuess = self.transformWGStoMap.transform(self.featureToGuess.geometry().asPoint())
+
+            self.dockwidget.nameLabel.setText(unicode(self.featureToGuess.attribute("name")))
+
+            self.dockwidget.webView.setHtml("")
+            self.dockwidget.hintLabel.setText("")
+            wikiLang, wikiPage = self.featureToGuess.attribute("wikipedia").split(':')
+            self.dockwidget.webView.setUrl(QUrl('http://' + wikiLang + '.m.wikipedia.org/wiki/' + wikiPage))
+        else:
+            self.stop()
 
     def start(self):
         self.layer = self.iface.activeLayer()
         
         if self.layer:
+            self.score = 0
+            self.rounds = 4
+
+            self.dockwidget.scorePanel.display(self.score)
+
             self.iface.mapCanvas().setMapTool(self.mapToolPoint)
+            self.iface.mapCanvas().xyCoordinates.connect(self.hint)
 
             self.setNextFeature()
 
             self.dockwidget.startButton.setEnabled(False)
             self.dockwidget.stopButton.setEnabled(True)
             self.mapToolPoint.canvasClicked.connect(self.guess)
+        else:
+            self.dockwidget.hintLabel.setText("First select a vector layer in the legend tree.")
 
     def stop(self):
         self.layer = None
+        self.featureToGuess = None
+        self.pointToGuess = None
+
         self.iface.mapCanvas().setMapTool(self.mapToolPan)
+        self.iface.mapCanvas().xyCoordinates.disconnect(self.hint)
 
         self.dockwidget.startButton.setEnabled(True)
         self.dockwidget.stopButton.setEnabled(False)
         self.dockwidget.nameLabel.setText("")
+        self.dockwidget.hintLabel.setText("")
+        self.dockwidget.webView.setHtml("")
         self.mapToolPoint.canvasClicked.disconnect()
 
     def guess(self, point):
-        print(point.x(), point.y())
+        if self.pointToGuess:
+            dist = self.pointToGuess.distance(point)
+            score = max(int(1000-dist)/4, 0)
+
+            self.score += score
+            self.dockwidget.scorePanel.display(self.score)
+            self.setNextFeature()
+
+    def hint(self, point):
+        if self.pointToGuess:
+            dist = self.pointToGuess.distance(point)
+
+            if dist > 2000:
+                self.dockwidget.hintLabel.setText('Are you even trying?!')
+            elif dist > 1000:
+                self.dockwidget.hintLabel.setText('Still some room for improvement..')
+            elif dist > 500:
+                self.dockwidget.hintLabel.setText('Close but no cigar!')
+            elif dist > 250:
+                self.dockwidget.hintLabel.setText('Gotcha!')
+        else:
+            self.dockwidget.hintLabel.setText("")
+
